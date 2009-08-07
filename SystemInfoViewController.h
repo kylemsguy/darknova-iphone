4e5029136c92b6e79f06104c6f4c59f3443cd0e5
doctorduke <doctorduke@75fdeefe-41a5-11de-b466-9154fc769079>
2009-08-07 06:37:08 +0000

    Reverted to working copy without webview

--- SystemInfoViewController.h
+++ SystemInfoViewController.h
@@ -1,16 +1,33 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
-@interface SystemInfoViewController : UIViewController<UINavigationBarDelegate> {
+@interface SystemInfoViewController : UIViewController<UINavigationBarDelegate>/* Specify a superclass (eg: NSObject or NSView) */ {
+	IBOutlet	UILabel*	systemName;
+	IBOutlet	UILabel*	systemSize;
+	IBOutlet	UILabel*	systemTechLevel;
+	IBOutlet	UILabel*	systemGoverment;
+	IBOutlet	UILabel*	systemResources;
+	IBOutlet	UILabel*	systemPolice;	
+	IBOutlet	UILabel*	systemPirates;
 	IBOutlet	UILabel*	systemMessage;	
-	IBOutlet    UIWebView*  systemInfoContent;
 	
 	IBOutlet	UIButton*	systemNews;
 	IBOutlet	UIButton*	systemHire;
 	IBOutlet	UIButton*	systemSpecial;	
 }
 
+/*@property (nonatomic, retain)	UILabel*	systemName;
+@property (nonatomic, retain)	UILabel*	systemSize;
+@property (nonatomic, retain)	UILabel*	systemTechLevel;
+@property (nonatomic, retain)	UILabel*	systemGoverment;
+@property (nonatomic, retain)	UILabel*	systemResources;
+@property (nonatomic, retain)	UILabel*	systemPolice;	
+@property (nonatomic, retain)	UILabel*	systemPirates;
+@property (nonatomic, retain)	UILabel*	systemMessage;	*/
 
+
+
+-(IBAction)testClick;
 -(IBAction)doQuests;
 -(IBAction)showNews;
 -(IBAction)showHire;
